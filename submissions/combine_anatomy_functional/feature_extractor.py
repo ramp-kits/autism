0e9693931f66f2fca46a5a7c71e1767e83b375e1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-04-26 10:23:58 +0200

    FIX seperate fit and transformer in transformer

--- submissions/combine_anatomy_functional/feature_extractor.py
+++ submissions/combine_anatomy_functional/feature_extractor.py
@@ -16,18 +16,21 @@ def _load_fmri(fmri_filenames):
 
 
 class FeatureExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        # make a transformer which will load the time series and compute the
+        # connectome matrix
+        self.transformer_fmri = make_pipeline(
+            FunctionTransformer(func=_load_fmri, validate=False),
+            ConnectivityMeasure(kind='tangent', vectorize=True))
+
     def fit(self, X_df, y):
+        fmri_filenames = X_df['fmri_msdl']
+        self.transformer_fmri.fit(fmri_filenames, y)
         return self
 
     def transform(self, X_df):
-        # get only the time series for the MSDL atlas
         fmri_filenames = X_df['fmri_msdl']
-        # make a transformer which will load the time series and compute the
-        # connectome matrix
-        transformer_fmri = make_pipeline(
-            FunctionTransformer(func=_load_fmri, validate=False),
-            ConnectivityMeasure(kind='tangent', vectorize=True))
-        X_connectome = transformer_fmri.fit_transform(fmri_filenames)
+        X_connectome = self.transformer_fmri.transform(fmri_filenames)
         X_connectome = pd.DataFrame(X_connectome, index=X_df.index)
         X_connectome.columns = ['connectome_{}'.format(i)
                                 for i in range(X_connectome.columns.size)]
--- submissions/starting_kit_functional/feature_extractor.py
+++ submissions/starting_kit_functional/feature_extractor.py
@@ -16,15 +16,19 @@ def _load_fmri(fmri_filenames):
 
 
 class FeatureExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        # make a transformer which will load the time series and compute the
+        # connectome matrix
+        self.transformer_fmri = make_pipeline(
+            FunctionTransformer(func=_load_fmri, validate=False),
+            ConnectivityMeasure(kind='tangent', vectorize=True))
+
     def fit(self, X_df, y):
+        # get only the time series for the MSDL atlas
+        fmri_filenames = X_df['fmri_msdl']
+        self.transformer_fmri.fit(fmri_filenames, y)
         return self
 
     def transform(self, X_df):
-        # get only the time series for the MSDL atlas
         fmri_filenames = X_df['fmri_msdl']
-        # make a transformer which will load the time series and compute the
-        # connectome matrix
-        transformer_fmri = make_pipeline(
-            FunctionTransformer(func=_load_fmri, validate=False),
-            ConnectivityMeasure(kind='tangent', vectorize=True))
-        return transformer_fmri.fit_transform(fmri_filenames)
+        return self.transformer_fmri.transform(fmri_filenames)
